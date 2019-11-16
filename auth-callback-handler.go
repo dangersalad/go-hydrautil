92b6773f670d18b517143622014f32dba77f59ea
paulbdavis <paul@dangersalad.com>
2019-11-16 11:03:44 -0700

    send json instead if ClientConfig.CookieName is empty

--- auth-callback-handler.go
+++ auth-callback-handler.go
@@ -14,75 +14,71 @@ func AuthCallbackHandler(oauthConf *oauth2.Config, clientConf ClientConfig) http
 	getState := getStateFunc(clientConf)
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		referrer, err := url.Parse(r.Header.Get("referrer"))
-		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			w.Write([]byte("invalid referror"))
-			return
-		}
-
 		code := r.URL.Query().Get("code")
 		// if no code, return error
 		if code == "" {
-			w.WriteHeader(http.StatusBadRequest)
-			w.Write([]byte("code required"))
+			sendErrorMessage(w, http.StatusBadRequest, "code required")
 			return
 		}
 		scope := r.URL.Query().Get("scope")
 		// if no challenge, return error
 		if scope == "" {
-			w.WriteHeader(http.StatusBadRequest)
-			w.Write([]byte("scope required"))
+			sendErrorMessage(w, http.StatusBadRequest, "scope required")
 			return
 		}
 		state := r.URL.Query().Get("state")
 		// if no challenge, return error
 		if scope == "" {
-			w.WriteHeader(http.StatusBadRequest)
-			w.Write([]byte("state required"))
+			sendErrorMessage(w, http.StatusBadRequest, "state required")
 			return
 		}
 		if state != getState(r) {
-			w.WriteHeader(http.StatusUnauthorized)
-			w.Write([]byte("state mismatch"))
+			sendErrorMessage(w, http.StatusUnauthorized, "state mismatch")
 			return
 		}
 		token, err := oauthConf.Exchange(r.Context(), code)
 		if err != nil {
 			if oauthErr, ok := err.(*oauth2.RetrieveError); ok {
-				w.WriteHeader(http.StatusUnauthorized)
-				w.Write(oauthErr.Body)
+				sendErrorMessage(w, http.StatusUnauthorized, string(oauthErr.Body))
 				return
 			}
-			w.WriteHeader(http.StatusInternalServerError)
-			w.Write([]byte(err.Error()))
+			sendErrorMessage(w, http.StatusInternalServerError, err.Error())
 			return
 		}
 		if token == nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			w.Write([]byte("token is nil"))
+			sendErrorMessage(w, http.StatusInternalServerError, "token is nil")
 			return
 		}
 
-		secure := referrer.Scheme == "https"
+		if clientConf.CookieName != "" {
+			referrer, err := url.Parse(r.Header.Get("referrer"))
+			if err != nil {
+				sendErrorMessage(w, http.StatusBadRequest, "invalid referror")
+				return
+			}
 
-		cookieDomain := r.Host
-		if r.Header.Get("x-cookie-domain") != "" {
-			cookieDomain = r.Header.Get("x-cookie-domain")
-		} else if r.Header.Get("host") != "" {
-			cookieDomain = r.Header.Get("host")
+			secure := referrer.Scheme == "https"
+
+			cookieDomain := r.Host
+			if r.Header.Get("x-cookie-domain") != "" {
+				cookieDomain = r.Header.Get("x-cookie-domain")
+			} else if r.Header.Get("host") != "" {
+				cookieDomain = r.Header.Get("host")
+			}
+
+			debugf("assigning token to cookie %s for domain %s: %#v\n", clientConf.CookieName, cookieDomain, token)
+
+			http.SetCookie(w, &http.Cookie{
+				Name:     clientConf.CookieName,
+				Value:    token.AccessToken,
+				HttpOnly: true,
+				Secure:   secure,
+				Domain:   cookieDomain,
+				Path:     "/",
+			})
+			return
 		}
 
-		debugf("assigning token to cookie %s for domain %s: %#v\n", clientConf.CookieName, cookieDomain, token)
-
-		http.SetCookie(w, &http.Cookie{
-			Name:     clientConf.CookieName,
-			Value:    token.AccessToken,
-			HttpOnly: true,
-			Secure:   secure,
-			Domain:   cookieDomain,
-			Path:     "/",
-		})
-
+		sendJSON(w, token)
 	})
 }
